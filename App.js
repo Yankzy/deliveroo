2e0fb1dcf3b0c92b5e1a1f0a0a0b6d6898eb5641
Yankzy <qubzen@gmail.com>
2023-03-31 19:32:25 +0000

    changed prompt template

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './screens/Home';
+import Restaurant from './screens/Restaurant';
+import { Provider } from 'react-redux'
+import Basket from './components/Basket';
+import store from './features/Store';
 
-export default function App() {
+
+const Stack = createNativeStackNavigator();
+
+const App = () =>{
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{ headerShown: false }}
+          initialRouteName="Home"
+        >
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Restaurant" component={Restaurant} />
+          <Stack.Screen 
+            name="Basket" 
+            component={Basket} 
+            options={{presentation: 'modal'}} 
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
